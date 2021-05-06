0c559e850753883cb8535cdd2b6b72dfaff8c175
Håvard Stigen Andersen <havard.stigen.andersen@nav.no>
2021-05-06 07:39:35 +0200

    Log hvilke origins vi bruker

--- src/main/kotlin/no/nav/helse/flex/gateway/cors/CorsConfig.kt
+++ src/main/kotlin/no/nav/helse/flex/gateway/cors/CorsConfig.kt
@@ -1,16 +1,25 @@
 package no.nav.helse.flex.gateway.cors
 
+import no.nav.helse.flex.gateway.logger
 import org.springframework.beans.factory.annotation.Value
 import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
 import org.springframework.web.cors.CorsConfiguration
 import org.springframework.web.cors.reactive.CorsWebFilter
 import org.springframework.web.cors.reactive.UrlBasedCorsConfigurationSource
+import javax.annotation.PostConstruct
 
 @Configuration
 class CorsConfig(@Value("\${allowed.origins}") private val allowedOrigins: String) {
 
     private val allowedOriginsList: List<String> = allowedOrigins.split(",")
+
+    val log = logger()
+
+    @PostConstruct
+    fun postContruct() {
+        log.info("Allowed origins $allowedOriginsList")
+    }
 
     @Bean
     fun corsWebFilter(): CorsWebFilter {
